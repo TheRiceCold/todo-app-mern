8ce8bda4ab1affb917e3cef0cc70f5522e2bb7f0
Dale Walter <dalewaltergh@gmail.com>
2022-08-03 02:53:57 +0800

    get todos from database

--- server/routes/todos.js
+++ server/routes/todos.js
@@ -1,13 +1,21 @@
 const router = require("express").Router();
-const TodoModel = require("../models/todo");
+const todoModel = require("../models/todo");
 
 module.exports = router
   .post("/api/todos", async(req, res) => {
     try {
-      const newTodo = new TodoModel({ task: req.body.task });
+      const newTodo = new todoModel({ task: req.body.task });
       await newTodo.save();
       res.status(200).json("Todo task added successfully!");
     } catch (error) {
       res.json(error);
     }
-  });
+  })
+  .get("/api/todos", async(req, res) => {
+    try {
+      const todos = await todoModel.find({});
+      res.status(200).json(todos);
+    } catch (error) {
+      res.json(error);
+    }
+  })
